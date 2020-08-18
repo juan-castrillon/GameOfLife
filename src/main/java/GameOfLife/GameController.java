c445926649b8a092f2b0fc6b960f310c5d8fde76
juan-castrillon <castrillon.95@gmail.com>
2020-08-18 17:34:37 +0200

    Applied MVC pattern and added build capabilities to gradle script

--- src/main/java/GameOfLife/GameController.java
+++ src/main/java/GameOfLife/GameController.java
@@ -1,13 +1,27 @@
 package GameOfLife;
 
+import GameOfLife.UI.GUI;
+
 public class GameController {
-    private final Universe model;
-    //private UniverseView view;
+    private Universe model;
+    private GUI view;
 
 
     public GameController(int sizeOfUniverse) {
         this.model = new Universe(sizeOfUniverse);
-        //this.view = new UniverseView();
+        this.view = new GUI();
+    }
+    public void reset(int size) {
+        this.model = new Universe(size);
+        this.view.setSizeOfUniverse(size);
+        this.view.reset();
+    }
+    public void delay() {
+        try {
+            Thread.sleep(this.speed());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean[][] currentState() {
@@ -18,9 +32,27 @@ public class GameController {
         this.model.evolve();
     }
     public int alive() {return this.model.getAlive();}
-    /*
-    public void updateView() {
-        this.view.show(this.model.getState());
-    }*/
+
+    public boolean isPlaying(){
+        return this.view.isPlaying();
+    }
+    public boolean isReset() {
+        return  this.view.isReset();
+    }
+    public boolean isNewGame() {return this.view.isNewGame();}
+
+    public int speed() {
+        return this.view.getSpeed();
+    }
+    public int size(){return this.view.getSizeOfUniverse();}
+    public int newSize() {return this.view.getNewSize();}
+
+    public void updateShowNewGame(){
+        this.view.showNewGame(this.view.isShowNewGame());
+    }
+
+    public void updateView(int gen, int alive, boolean[][] state) {
+        this.view.update(gen, alive, state);
+    }
 
 }
